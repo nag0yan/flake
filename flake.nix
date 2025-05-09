28922695beae2fb0717eb8f2de2a33e95039c00a
nag0yan <matsui-ka@kadokawa.jp>
2025-05-09 13:59:20 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  "description" = "flake templates";
+  "inputs" = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  "outputs" = { nixpkgs, self }: {
+    templates = {
+      basic = {
+        path = ./basic;
+        description = "A basic flake which can be used by any platform";
+      };
+
+      defaultTemplate = self.templates.basic;
+    };
+
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+  };
+}
