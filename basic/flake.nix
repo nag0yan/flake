26a628398383f6dac8283c1b3f1f2e6dd09270d8
nag0yan <matsui-ka@kadokawa.jp>
2025-05-09 14:27:10 +0900

    fix: use mkShell

--- basic/flake.nix
+++ basic/flake.nix
@@ -1,20 +1,22 @@
 {
+  description = "A very basic flake";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages = {
-          hello = pkgs.hello;
-        };
-      }
-    );
+  outputs = { nixpkgs, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem(
+    system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.default = pkgs.mkShell {
+        packages = with pkgs; [
+        ];
+      };
+    }
+  );
 }
